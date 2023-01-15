24907143fed08c8e94f14cffa6394b9c7cea6dba
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-15 11:25:36 -0300

    Git Context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import { faTerminal } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import Cover from "../../assets/Cover.png";
 export function Header() {
   return (
-    <header>
-      <FontAwesomeIcon icon={faTerminal} />
-      <h1>GITHUB BLOG</h1>
+    <header className="w-screen">
+      <img className="w-full" src={Cover} alt="" />
+      {/* <FontAwesomeIcon icon={faTerminal} />
+      <h1>GITHUB BLOG</h1> */}
     </header>
   );
 }
